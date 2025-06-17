ceae23e12ea86cedbd2fc3ce8d1876b6c71e65c7
sakthi-2005 <sakthikumar.ask777@gmail.com>
2025-06-17 08:56:13 +0530

    choas: changed directory name

--- backend/routes/addUser.js
+++ backend/routes/addUser.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const queue = require("../utils/queue");
+const queue = require("../queues/queue");
 
 router.post("/addUser", queue);
 
